157b815b64a28c41d09a74e8c2872bb1e9f64b41
Vexify4103 <painmain.shadow@gmail.com>
2025-07-29 02:22:34 +0200

    2.9.0

--- src/events/discord/ready.js
+++ src/events/discord/ready.js
@@ -4,12 +4,12 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 
-	execute(client) {
+	async execute(client) {
 		// Deploy slash commands
 		require("../../deploy-commands")(client);
 
 		// Initialize Lavalink
-		client.manager.init({ clientId: client.user.id });
+		await client.manager.init({ clientId: client.user.id });
 
 		// Print a message to the console indicating that the client is ready
 		console.log(`[INFO] Ready! Logged in as ${client.user.tag}`);
