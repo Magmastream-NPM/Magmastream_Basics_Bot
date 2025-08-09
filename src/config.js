ae259a4f5cd66e54ca1e7b2c5ae9126d47574d95
Vexify4103 <47192617+Vexify4103@users.noreply.github.com>
2025-08-09 12:53:31 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
 			maxRetryAttempts: 500, // optional
 			retryDelayMs: 300000, // optional
 			enableSessionResumeOption: true, // optional
-			sessionTimeoutMs: 300, // optional
+			sessionTimeout: 300, // optional
 		},
 	],
 };
